56da96e655b6cea537fc1501957371d37fd94489
unknown <salesbass@google.com>
2020-09-22 00:49:58 +0200

    Adding checout page and checkout item component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from "react-redux";
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selector';
+import { createStructuredSelector } from "reselect";
+import CheckoutPage from './pages/checkout/checkout.component';
 
 class App extends React.Component {
 
@@ -40,6 +43,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignOutPage />)} />
         </Switch>
       </div>
@@ -47,8 +51,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
