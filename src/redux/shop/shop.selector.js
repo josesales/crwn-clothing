3af7dc1bcceb6552bdf9249dcd317ce57a442727
unknown <salesbass@google.com>
2020-10-02 13:05:33 +0200

    Moving directory and shop data to its on reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,14 @@
+import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
+
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector([selectShop], shop => shop.collections);
+
+//Object.keys(shop.collections): Convert the keys of a hash map object into an array 
+export const selectCollectionsForPreview = createSelector([selectCollections], collections => Object.keys(collections)
+    .map(key => collections[key]));
+
+// selectCollection does not ge memoized due to the collectionUrlParam which is passed dynamically 
+// so to memoize it we have to wrap the function in the memoize method
+export const selectCollection = memoize(collectionUrlParam => createSelector([selectCollections], collections => collections[collectionUrlParam]));
